bffb0b479dfaaf18513439b3cebcfb5fab10b8a7
sanket.acharya <sanket.acharya4@hotmail.com>
2024-04-16 17:54:41 +0545

    changes in intercommunication for getCountry and getCurrency

--- src/main/java/com/swifttech/promocodeservice/util/InterCommunication.java
+++ src/main/java/com/swifttech/promocodeservice/util/InterCommunication.java
@@ -7,12 +7,14 @@ import com.swifttech.promocodeservice.core.model.ApiError;
 import com.swifttech.promocodeservice.core.model.ApiResponse;
 import com.swifttech.promocodeservice.core.records.CodeRecord;
 import com.swifttech.promocodeservice.payload.request.CountryApiRequest;
+import com.swifttech.promocodeservice.payload.response.CountryResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
@@ -38,6 +40,45 @@ public class InterCommunication {
                });
 
    }
+    public CountryResponse getCountry(UUID countryId) {
+        try {
+            log.info("Fetching Country by country id");
+            String uri = UriComponentsBuilder.fromUriString(masterService)
+                    .path("country" + FOLDER_SEPERATOR + countryId.toString())
+                    .build()
+                    .toString();
 
+            return webClient.build()
+                    .get()
+                    .uri(uri)
+                    .retrieve()
+                    .onStatus(HttpStatusCode::isError, InterCommunication::apply)
+                    .bodyToMono(CountryResponse.class)
+                    .block();
+        } catch (Exception e) {
+            log.error("Failed to fetch country by countryId");
+            throw new RemitException(codes.pick("CON001"));
+        }
+    }
+    public CurrencyResponse getCurrency(UUID currencyId){
+       try {
+           String uri = UriComponentsBuilder.fromUriString(masterService)
+                   .path("country" + FOLDER_SEPERATOR + countryId.toString())
+                   .build()
+                   .toString();
+
+           return webClient.build()
+                   .get()
+                   .uri(uri)
+                   .retrieve()
+                   .onStatus(HttpStatusCode::isError, InterCommunication::apply)
+                   .bodyToMono(CountryResponse.class)
+                   .block();
+       } catch (Exception e) {
+           log.error("Failed to fetch country by countryId");
+           throw new RemitException(codes.pick("CON001"));
+       }
+       }
+    }
 
 }
